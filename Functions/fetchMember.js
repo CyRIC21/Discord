64a4059bc4f72b56bf9fd5b93d35a5cb26614de6
Howesy <chazlikeskfc@gmail.com>
2018-05-04 15:30:21 +0100

    [Updated] Added Explanation

--- Functions/fetchMember.js
+++ Functions/fetchMember.js
@@ -11,3 +11,12 @@ function fetchMember(userResolveable) {
 const mentionedUser = msg.mentions.users.first();
 if (!mentionedUser) return msg.reply("No mentioned user!");
 fetchMember(mentionedUser).then(m => m.ban()).catch(error => console.error(error));
+
+/*
+You could possibly place this function in a module and append that module to your client variable as a means of easy access to fetch a user.
+Understand that you have to append the module to where your client is declared.
+You append modules to the client variable as such:
+require("./myFunctions.js)(yourClientVariable);
+Then you can simply call the fetch member as such anyway that calls the client variable.
+yourClientVariable.fetchMember(userResolveable).then(m => m.ban()).catch(error => console.error(error));
+*/
