4517a1378f8836d5b5b4372e95448470424c79e0
Howesy <chazlikeskfc@gmail.com>
2018-05-04 21:14:29 +0100

    [Updated] Added Separate Properties
    Added the [memberAmount] variable from the promise, as well as the raw collection.

--- Functions/fetchRoleMembers.js
+++ Functions/fetchRoleMembers.js
@@ -1,18 +1,24 @@
 function fetchRoleMembers(roleName) {
-  return new Promise(function(resolve, reject) {
-    const roleString = roleName.toString();
-    const fetchedRole = msg.guild.roles.find("name", roleString);
-    if (fetchedRole && fetchedRole.members.size !== 0) resolve(fetchedRole.members);
-    else reject("I couldn't find that role or no members were assigned to it!");
-  });
-}
-
-//Example:
-fetchRoleMembers("VIP").then(members => {
-  msg.channel.send(`The VIP role has \`${members.size}\` in it!`);
-}).catch(error => console.error(error));
-
-/*
-All this function does is simply returns the promise of a collection from your desired role.
-It has all the neccesary checks in place to make sure that you do not error during your function.
-*/
+    return new Promise(function(resolve, reject) {
+      const roleString = roleName.toString();
+      const fetchedRole = msg.guild.roles.find("name", roleString);
+      const restructuredObject = {
+        memberCollection: fetchedRole.members,
+        memberAmount: fetchedRole.members.size,
+      };
+      if (fetchedRole && fetchedRole.members.size !== 0) resolve(restructuredObject);
+      else reject("I couldn't find that role or no members were assigned to it!");
+    });
+  }
+  
+  //Example:
+  fetchRoleMembers("VIP").then(role => {
+    msg.channel.send(`The VIP role has \`${role.memberAmount}\` in it!`);
+    msg.channel.send(`The VIP role members are \`${role.memberCollection.map(m => m.displayName).join("\n")}\``);
+  }).catch(error => console.error(error));
+  
+  /*
+  All this function does is simply returns the amount of members in that role, or just the raw collection.
+  It has all the neccesary checks in place to make sure that you do not error during your function.
+  */
+  
