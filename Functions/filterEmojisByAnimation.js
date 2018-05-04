8eecde4f37c82bd5222ebbc05ec351005af9431d
Howesy <chazlikeskfc@gmail.com>
2018-05-04 22:52:48 +0100

    [Create] Filter Emojis By Animation

---
+++ Functions/filterEmojisByAnimation.js
@@ -0,0 +1,19 @@
+function filterEmojisByAnimation() {
+  return new Promise(function(resolve, reject) {
+    let filteredEmojis = msg.guild.emojis.filter(e => e.animated == true);
+    if (filteredEmojis.size != 0) resolve(filteredEmojis);
+    else reject("There are no animated emojis in this server!");
+  });
+};
+
+//Example:
+filterEmojisByAnimation().then(emojis => {
+    msg.channel.send(`Animated Emojis include: ${emojis.map(e => e.name).join("\n")}`);
+}).catch(error => console.error(error));
+
+/*
+This function literally returns a collection of emojis that are specifically animated.
+This function is intended simply for the fact of cool emojis.
+*/
+
+//Returns: Promise<Collection[Emojis]>
