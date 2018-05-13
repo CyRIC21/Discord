5d664b8058383b619816533e0fb239fcdeb78e06
Howesy <chazlikeskfc@gmail.com>
2018-05-13 18:56:20 +0100

    Add files via upload

--- Commands/purge.js
+++ Commands/purge.js
@@ -1,12 +1,12 @@
 exports.run = function(bot, msg, args) {
-    var messageAmount = args[0];
-    if (!messageAmount) return bot.embed(msg, 0xff0000, "Invalid amount specification:", "Specify how many messages you want to delete.");
-    if (isNaN(messageAmount)) return bot.embed(msg, 0xff0000, "Invalid amount specification:", "Please enter a number!");
-    var parsedMessageAmount = parseInt(messageAmount);
-    if (parsedMessageAmount > 100) return bot.embed(msg, 0xff0000, "Invalid amount specified!", "Amount needs to be less than or equal to 100 to purge.");
-    msg.channel.fetchMessages({limit: parsedMessageAmount});
-    msg.channel.bulkDelete(parsedMessageAmount, true);
-    bot.embed(msg, 0xff0000, "Successfully purged messages!", `Purged: ${messageAmount} messages!`);
+  var messageAmount = args[0];
+  if (!messageAmount) return bot.embed(msg, 0xff0000, "Invalid amount specification:", "Specify how many messages you want to delete.");
+  var parsedMessageAmount = parseInt(messageAmount);
+  if (parsedMessageAmount < 0) return bot.embed(msg, 0xff0000, "Invalid amount specified!", "Amount needs to be more than 0 to prune!"); 
+  if (parsedMessageAmount > 100) return bot.embed(msg, 0xff0000, "Invalid amount specified!", "Amount needs to be less than or equal to 100 to purge.");
+  msg.channel.fetchMessages({limit: parsedMessageAmount});
+  msg.channel.bulkDelete(parsedMessageAmount, true);
+  bot.embed(msg, 0xff0000, "Successfully purged messages!", `Purged: ${parsedMessageAmount} messages!`);
 };
 
 exports.conf = {
